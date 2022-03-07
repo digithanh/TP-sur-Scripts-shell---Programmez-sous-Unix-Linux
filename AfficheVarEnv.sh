9fc18bc553d2a5b610e6444f3fa493e1169af43c
digithanh <tc.nguyen@live.com>
2022-03-07 05:32:35 +0100

    initial commit

---
+++ AfficheVarEnv.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Répertoire de connexion : $HOME"
+echo "Liste des répertoire de recherche de commandes : $PATH"
+echo "Nom d'utilisateur : $USER"
+echo "Répertoire courant : $PWD"
+exit 0
